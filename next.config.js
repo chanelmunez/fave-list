2330dc4456fbf4c9c23374d80cf9552e758e04b0
chanel munezero <chanelmunez@gmail.com>
2025-01-05 10:01:26 -0600

    -amupdate to use next-on-page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /**
  * @type {import('next').NextConfig}
- */
+ * /
 const nextConfig = {
     env: {
         stackbitPreview: process.env.STACKBIT_PREVIEW
@@ -10,3 +10,16 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+*/
+
+
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+
+export default nextConfig;
